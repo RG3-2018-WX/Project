d572c05c2bb9f68d1ff0fe7eec5ea9c5e3a67d3d
panhanlin16 <40332634+panhanlin16@users.noreply.github.com>
2018-11-24 18:08:03 +0800

    Add files via upload

---
+++ DjangoProject/userpage/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+from django.conf.urls import url
+
+from userpage.views import *
+
+
+
+
+urlpatterns = [
+
+    url(r'^activity/list/?$', ActivityList.as_view()),
+    url(r'^activity/detail/?$', ActivityDetail.as_view()),
+    url(r'^activity/program/?$', ProgramDetail.as_view()),
+    url(r'^lottery/list/?$', LotteryInfo.as_view())
+]
