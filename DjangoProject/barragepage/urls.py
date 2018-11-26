563dfbc052b4debfa546dae15cff269ceb4080a9
panhanlin16 <40332634+panhanlin16@users.noreply.github.com>
2018-11-26 19:56:05 +0800

    Update urls.py

--- DjangoProject/barragepage/urls.py
+++ DjangoProject/barragepage/urls.py
@@ -6,4 +6,8 @@ from adminpage.views import *
 
 
 urlpatterns = [
-    ]
+    url(r'^comment/top/?$', SetTop.as_view()),
+    url(r'^comment/now/?$', SetComment.as_view()),
+    url(r'^comment/linenumber/?$', CommentLinenumber.as_view())
+]
+    
