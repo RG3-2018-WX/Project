d9c5f6b241b4112ecaef8c87d638df53a6708649
panhanlin16 <40332634+panhanlin16@users.noreply.github.com>
2018-11-30 14:07:38 +0800

    Update views.py

--- DjangoProject/userpage/views.py
+++ DjangoProject/userpage/views.py
@@ -2,30 +2,36 @@ from django.shortcuts import render
 from codex.baseerror import *
 from codex.baseview import APIView
 from django.utils import timezone
-from wechat.models import User,Activity,Program,LotteryResult
+from DjangoProject.models import ActivityUser,Activity,Program,LotteryResult,Comment, Barrage, Picture
 import json
 import datetime
 import re
 
 class ActivityList(APIView):
- def get(self):
-        show_list = ActivityUser.activitySelcetedByUser(self.input['open_id'])
+    def get(self):
+        show_list = ActivityUser.activitySelcetedByUser(self.input['openId'])
         list = []
-        for i in show_list:
+        for activity in show_list:
             list.append(
                 {
-                    'name': i.name,
-                    'description': i.description,
-                    'startTime':i.start_time,
-                    'place': i.place,
-                    'endTime':i.end_time
+                    'name': activity.name,
+                    'description': activity.description,
+                    'startTime':activity.start_time,
+                    'place': activity.place,
+                    'endTime':activity.end_time
                 }
             )
-        return list
+        if list:
+            return list
+        else:
+            raise InputError('the user attend no activity')
+
 class ActivityDetail(APIView):
     def get(self):
         self.check_input('activityId')
         program_list = Programe.selectByActivity(Activity.selectById(self.input['activityId']))
+        if not program_list:
+            raise InputError('no such activity')
         show_list = []
         for program in program_list:
             show_list.append(
@@ -40,6 +46,8 @@ class ProgramDetail(APIView):
     def get(self):
         self.check_input('sequence','activityId')
         program = Program.objects.get(activity__id=input(['activityId']),sequence=input(['sequence']))
+        if not program:
+            raise InputError('no such program')
         show = {
             'name':program.name,
             'description':program.description,
@@ -48,14 +56,29 @@ class ProgramDetail(APIView):
         return show
 class LotteryInfo(APIView):
     def get(self):
-        self.check_input('openId','lotteryId')
-        lottery_result = LotteryResult.objects.get(open_id=self.input['openId'],lottery__id=self.input['lotteryId'])
+        self.check_input('openId','activityId')
+        lottery_result = LotteryResult.objects.filter(open_id=self.input['openId'],lottery__activity__id=self.input['activityId'])
         if lottery_result :
-            info = lottery_result.prize
-            return info
+            show_list = []
+            for i in lottery_result:
+                show_list.append({
+                    'name': i.lottery.name,
+                    'prize':i.prize
+                })
         else:
-            raise ValidateError('thank you for attendence')
+            raise InputError('thank you for attendance')
+class SetComment(APIView):
+    def post(self):
+        self.check_input('openId')
+        Comment.insertComment(Activity.selectById(self.input['activityId']), self.input['openId'],
+                              self.input['color'], self.input['content'],
+                              self.input['bolt'], self.input['underline'], self.input['incline'], timezone.now(),
+                              Barrage.OK)
 
 
-
-# Create your views here.
+class SetPicture(APIView):
+    def post(self):
+        self.check_input('openId')
+        Picture.insertComment(Activity.selectById(self.input['activityId']), self.input['openId'], self.input['picUrl'],
+                              timezone.now(),
+                              Barrage.OK)
