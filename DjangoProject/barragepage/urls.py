4d1f6717bc85582d91b2512a706e4ac56fd6aa1b
panhanlin16 <40332634+panhanlin16@users.noreply.github.com>
2018-11-30 14:04:54 +0800

    Update urls.py

--- DjangoProject/barragepage/urls.py
+++ DjangoProject/barragepage/urls.py
@@ -1,13 +1,15 @@
+# -*- coding: utf-8 -*-
+#
 from django.conf.urls import url
-from adminpage.views import *
 
+from barragepage.views import *
 
 
 
 
 urlpatterns = [
     url(r'^comment/top/?$', SetTop.as_view()),
-    url(r'^comment/now/?$', SetComment.as_view()),
+    url(r'^comment/comment/?$', SetComment.as_view()),
+    url(r'^comment/picture/?$', SetPicture.as_view()),
     url(r'^comment/linenumber/?$', CommentLinenumber.as_view())
 ]
-    
