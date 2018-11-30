fe31ffac4735d6161c2c47579493df8b67c3fe1d
panhanlin16 <40332634+panhanlin16@users.noreply.github.com>
2018-11-30 14:06:25 +0800

    Update urls.py

--- DjangoProject/userpage/urls.py
+++ DjangoProject/userpage/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     url(r'^activity/list/?$', ActivityList.as_view()),
     url(r'^activity/detail/?$', ActivityDetail.as_view()),
     url(r'^activity/program/?$', ProgramDetail.as_view()),
-    url(r'^lottery/list/?$', LotteryInfo.as_view())
+    url(r'^lottery/list/?$', LotteryInfo.as_view()),
+    url(r'^activity/comment/?$', SetComment.as_view()),
+    url(r'^activity/picture/?$', SetPicture.as_view())
 ]
