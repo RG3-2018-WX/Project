e41d071281d48f20c0ba853e59aa8eda8ceafb98
putianlihai <40923562+putianlihai@users.noreply.github.com>
2019-01-04 17:35:34 +0800

    图片路径修改

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ['*']
 
 #MEDIA_ROOT = os.getcwd()
 #MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/b/img/').replace('\\', '/')
-MEDIA_URL = 'templates/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/').replace('\\', '/')
+MEDIA_URL = 'static/'
 # Application definition
 
 INSTALLED_APPS = [
